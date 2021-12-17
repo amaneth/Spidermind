f74756bade0029be846a572d9705626d93d5f1a6
MilaBooot <spyxmeni@gmail.com>
2021-12-17 18:30:12 +0300

    aded configuration settings for the filter

--- crawler/apps.py
+++ crawler/apps.py
@@ -1,10 +1,10 @@
 from django.apps import AppConfig
 from django.db.models.signals import post_migrate
 
-
+DEFAULT_RELEARN_TIME=10
 def create_periodic_task(sender, **kwargs):
     from django_celery_beat.models import PeriodicTask, IntervalSchedule
-    schedule, created = IntervalSchedule.objects.get_or_create(every=10,
+    schedule, created = IntervalSchedule.objects.get_or_create(every=DEFAULT_RELEARN_TIME,
                 period=IntervalSchedule.SECONDS,
                 )
     PeriodicTask.objects.update_or_create(
@@ -15,7 +15,22 @@ def create_periodic_task(sender, **kwargs):
 def create_categories(sender, **kwargs):
     from crawler.models import Setting
     Setting.objects.update_or_create(section_name='category', setting_name='ai', setting_type=2,
-            defaults={'setting_value':"'AI artificial intelligence robotics robote natural-language-processing nlp machine-learning machine learning natural language processing  knowledge intelligent'"})
+            defaults={'setting_value':"AI artificial intelligence robotics robot natural-language-processing\
+                 nlp machine-learning GAI  intelligen"})
+
+    Setting.objects.update_or_create(section_name='category', setting_name='blockchain', setting_type=2,
+            defaults={'setting_value':"cryptographic blockchain token ethereum bitcoin\
+                                crypto crypto-cash crypto-currency online-transactions cyber-cash"})
+
+    Setting.objects.update_or_create(section_name='category', setting_name='computer', setting_type=2,
+            defaults={'setting_value':"pc computer laptop desktop netbook mobile tablet multiprocessor microprocessor \
+                           microcomputer mainframe supercomputer automation telecommunications electronics"})
+
+    Setting.objects.update_or_create(section_name='category', setting_name='programming', setting_type=2,
+            defaults={'setting_value':"Ahack coding software application programming natural-language-processing machine-learning\
+                              software java python ruby mysql php laravel robotics JavaScript C# arduino"})
+    
+
 
 class CrawlerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
@@ -23,4 +38,4 @@ class CrawlerConfig(AppConfig):
     verbose_name = "newspaper crawler"
     def ready(self):
         post_migrate.connect(create_periodic_task, sender=self)
-        post_migrate.connect(create_categories, sender=delf)
+        post_migrate.connect(create_categories, sender=self)

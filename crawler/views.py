3badcf4710d923d4be82954f2a930a02c9e794f2
aman <amankassahun190@gmail.com>
2021-12-20 14:44:47 +0300

    added crawler settings

--- crawler/views.py
+++ crawler/views.py
@@ -13,30 +13,20 @@ import nltk
 from nltk.stem import WordNetLemmatizer
 
 from rest_framework.response import Response
+from rest_framework import status
 from django.http import HttpResponse, JsonResponse
 from rest_framework.views import APIView
 from rest_framework.parsers import JSONParser 
 from crawler.models import Article,Setting
-from crawler.serializers import ArticleSerializer
-from crawler.utils.news import SNETnews
+from crawler.serializers import ArticleSerializer, SettingSerializer
+from crawler.utils.news import SNETnews, logger
 from django.db import IntegrityError
 from django_celery_beat.models import PeriodicTask, IntervalSchedule
 
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-loghandle = loghandlers.TimedRotatingFileHandler(
-                filename="SNETnews.log",
-                when='D', interval=1, backupCount=7,
-                encoding="utf-8")
-loghandle.setFormatter(
-    logging.Formatter("%(asctime)s %(message)s"))
-logger.addHandler(loghandle)
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 
-link_param = openapi.Parameter( 'link', in_=openapi.IN_QUERY, description='link to be crawled',
-        type=openapi.TYPE_STRING, )
 auto_refresh_param = openapi.Parameter( 'enable', in_=openapi.IN_QUERY,
         description='if set true auto refresh will be on',type=openapi.TYPE_BOOLEAN, )
 fetch_params = [openapi.Parameter('sort', in_=openapi.IN_QUERY, description= "Sort type",
@@ -61,8 +51,6 @@ catagory_params = [openapi.Parameter('category', in_=openapi.IN_QUERY,
                 description= "Top n catagory_articles to be returned", type= openapi.TYPE_STRING, ),]
 
 
-"""from crawler.models import News_ini  ####
-setting_news = News_ini.objects.all()"""
 
 snet= SNETnews('./news.ini') 
 
@@ -83,8 +71,8 @@ class Refresh(APIView):
 
     '''
     def get(self, request, format=None):
-        article_serialized= snet.download_news()
-        return Response(article_serialized.data)
+        snet.download_news()
+        return Response({"Message": "Refresh is in progress...  "})
 
 
 
@@ -198,15 +186,34 @@ class Trending(APIView):
 
 
 
-class ArticleCrawl(APIView):
-    @swagger_auto_schema(manual_parameters=[link_param],security=[],
-            responses={'400': 'Validation Error','200': ArticleSerializer})
-    def get(self, request):
-        count =0
-        if request.method == 'GET':
-            link = request.GET["link"]
-            article_serialized = snet.article_crawl(link)
-            return Response(article_serialized.data)
+class CrawlerSettings(APIView):
+    '''
+    setting value is according to the following
+    0:integer
+    1:boolean
+    2:string
+    3:float
+    4:list
+    5:dict
+    '''
+    @swagger_auto_schema(request_body=openapi.Schema(
+        type=openapi.TYPE_OBJECT,
+        properties={
+            'section_name': openapi.Schema(type=openapi.TYPE_STRING,
+                description='The section the setting is in'),
+            'setting_name': openapi.Schema(type=openapi.TYPE_STRING, description='The name of the setting'),
+            'setting_value': openapi.Schema(type=openapi.TYPE_STRING,
+                    description='The value of the setting'),
+            'setting_type': openapi.Schema(type=openapi.TYPE_INTEGER,
+                    description='The data type of the setting value'),
+        }))
+
+    def post(self, request):
+        serializer= SettingSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 
@@ -219,3 +226,4 @@ class AutoRefresh(APIView):
             periodic_task.enabled = start
             periodic_task.save()
             return Response({'enable':str(start)})
+

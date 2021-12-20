c1b542a588ef82a0c66891f9d745a99b28caefc0
aman <amankassahun190@gmail.com>
2021-12-20 14:16:40 +0300

    serializer for the Setting model

--- crawler/serializers.py
+++ crawler/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
 from crawler.models import Article
+from crawler.models import Setting
 
 
 class ArticleSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Article
-		fields = ['content_id','title', 'description','authors', 'date', 'link','top_image','keywords', 'source_type','source']
+		fields = ['content_id','title', 'description','authors', 'date', 'link','top_image','keywords',
+                'source_type','source']
+
+class SettingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Setting
+        fields = '__all__'
